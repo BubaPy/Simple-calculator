a5e8b4d47b6e5e8343045161d015879f808d600a
Omar-M-Z <omar.m.zaheer@gmail.com>
2021-09-11 12:25:38 +0300

    created basic ui

--- simple-calculator.py
+++ simple-calculator.py
@@ -1,37 +1,28 @@
 #author: BubaPy
+#author 2: Omar-M-Z
 
-#This part prints the chosing menu on the start
+import tkinter as tk
+from tkinter import *
 
-print("Add - 1")
-print("Subtract - 2")
-print("Multiply - 3")
-print("Divide - 4")
-chos = int(input("Please press 1 - 4 to begin! "))
+class Application():
+    def __init__(self, master):
+        #creating the window, setting the dimensions, and centering the window on the screen
+        self.master = master
+        master.title("New Year CountDown")
+        window_width = 300
+        window_height = 400
+        screen_width = root.winfo_screenwidth()
+        screen_height = root.winfo_screenheight()
+        center_x = int(screen_width/2 - window_width / 2)
+        center_y = int(screen_height/2 - window_height / 2)
+        master.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
+        self.add_widgets()
 
-#This part is for Addition
+    def add_widgets(self):
+        #stuff like buttons will be added here
+        pass
 
-if chos == 1:
-    add1 = float(input("Enter the first number to add: "))
-    add2 = float(input("Enter the second number to add: "))
-    print("Your total is: ", add1 + add2)
-
-#This part is for subtraction
-
-if chos == 2:
-    sub1 = float(input("Enter the first number to subtract: "))
-    sub2 = float(input("Enter the second number to subtract: "))
-    print("Your subtraction is: ", sub1 - sub2)
-
-#This part is for multiplication
-
-if chos == 3:
-    mul1 = float(input("Enter the first number to multiply: "))
-    mul2 = float(input("Enter the second number to multiply: "))
-    print("Your multiplication is: ", mul1 * mul2)
-
-#This part is for dividation
-
-if chos == 4:
-    div1 = float(input("Enter the first number to divide: "))
-    div2 = float(input("Enter the second number to divide: "))
-    print("Your dividation is: ", div1 / div2)
+root = Tk()
+root.resizable(False, False)
+application = Application(root)
+root.mainloop()
